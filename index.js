4e05a77c14788af7c8fca5e221fd0751b2d2f8f4
Rabbit Factory <jv16th.raj@gmail.com>
2025-08-08 16:38:12 +0600

    changed port

--- index.js
+++ index.js
@@ -43,6 +43,9 @@ app.post('/search', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('Server started on http://localhost:5000');
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
+
